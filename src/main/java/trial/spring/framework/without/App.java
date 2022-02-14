0e11f992bca4e3410a5290c60fdf0c400d999b20
g-nabe <gakuwnabe@gmail.com>
2022-02-15 02:32:46 +0900

    without

---
+++ src/main/java/trial/spring/framework/without/App.java
@@ -0,0 +1,14 @@
+package trial.spring.framework.without;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		Message message = new Message();
+		message.setMessage("hello spring!");
+		System.out.println(message.getMessage());
+	}
+}
---
+++ src/main/java/trial/spring/framework/without/Message.java
@@ -0,0 +1,14 @@
+package trial.spring.framework.without;
+
+public class Message {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
